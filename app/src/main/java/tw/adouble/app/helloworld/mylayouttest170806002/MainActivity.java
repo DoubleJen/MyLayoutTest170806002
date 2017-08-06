dc68834cae9d99632ddb6a977ada5681f651b32b
Chang <qoo2003654@gmail.com>
2017-08-06 17:25:39 +0800

    170806 1725

--- app/src/main/java/tw/adouble/app/helloworld/mylayouttest170806002/MainActivity.java
+++ app/src/main/java/tw/adouble/app/helloworld/mylayouttest170806002/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView logText;
     private Boolean isWinner;
     private int counter;
+    private String[] items = {"2", "3", "4", "5", "6", "7"};
+    private int dig = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     //
     private void initGame(){
-        answer = createAnswer(3);
+        answer = createAnswer(dig);
         Log.d("brad", "answer is " + answer);
         isWinner = false;
         counter = 0;
@@ -43,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     //Create Answer
     private String createAnswer(int num){
         Set<Integer> set = new HashSet<>();
-        while(set.size()<3){
+        while(set.size()<num){
             set.add((int)(Math.random()*10));
         }
         StringBuilder sb = new StringBuilder();
@@ -68,12 +70,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void doGuess(View view){
         counter++;
-        String guess = inputEdit.getText().toString();
+        String guess = inputEdit.getText().toString();//加入檢查機制
         String result = checkAB(answer, guess);
         logText.append(counter + "." +guess + " : " + result + "\n");
         inputEdit.setText("");
 
-        if(result.equals("3A0B")){
+        if(result.equals(dig + "A0B")){
             //WINNER
             isWinner = true;
             showDialog();
@@ -101,18 +103,70 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doReset(View view){
-        // TODO doReset
+        AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Reset");
+        builder.setMessage("Yes or No");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                initGame();
+            }
+        });
+        builder.setNegativeButton("No", null);
+        dialog = builder.create();
+        dialog.show();
 
     }
 
     public void doSetting(View view){
-        // TODO doSetting
+        AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Guess Number");
+        builder.setCancelable(false);
+//        builder.setItems(items, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                dig = i + 2;
+//                initGame();
+//            }
+//        });
 
+        builder.setSingleChoiceItems(items, dig - 2, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dig = i + 2;
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                initGame();
+            }
+        });
+
+
+        dialog = builder.create();
+        dialog.show();
     }
 
     public void doExit(View view){
-        // TODO doExit
-
+        AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit");
+        builder.setMessage("Yes or No");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();//override finish make back not to exit
+            }
+        });
+        builder.setNegativeButton("No", null);
+        dialog = builder.create();
+        dialog.show();
     }
 
 }
